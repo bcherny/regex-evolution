35b48f7d334b33ed49e937d48f7b2567f2b04c9a
Boris Cherny <bcherny@gmail.com>
2016-09-04 01:10:50 -0700

    almost working...

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const CORRECTNESS_BONUS = 10;
 const INCORRECTNESS_PENALTY = -1;
 const INVALID_PENALTY = -100;
 
+const INITIAL_FITNESS = -Infinity
+
 const MUTATION_TYPES = {
   0: 'ADDITION',
   1: 'DELETION',
@@ -12,21 +14,40 @@ const MUTATION_TYPES = {
   3: 'DUPLICATION'
 }
 
+const maybe = fn => {
+  try {
+    return fn()
+  } catch (e) {
+    return undefined
+  }
+};
+
+// regex: string => RegExp
+function toRegex(regex) {
+  return maybe(() => new RegExp(regex))
+}
+
 // regex: string, cases: string[][] => number
 function getFitness(regex, cases) {
 
-  let rgx;
-  try {
-    rgx = new RegExp(regex);
-  } catch (e) {
-    return INVALID_PENALTY;
+  if (regex === '') {
+    return INITIAL_FITNESS
   }
+
+  let rgx = toRegex(regex);
+  console.log('rgx', rgx)
 
   return (
     cases[0].reduce((score, _) => score + (rgx.test(_) ? CORRECTNESS_BONUS : INCORRECTNESS_PENALTY), 0)
     + cases[1].reduce((score, _) => score + (rgx.test(_) ? INCORRECTNESS_PENALTY : CORRECTNESS_BONUS), 0)
     + COMPLEXITY_PENALTY * getComplexity(regex)
   );
+}
+
+// regex: string, cases: string[][] => number
+function getPercentCorrect(regex, cases) {
+  return cases[0].reduce((score, _) => score + (getFitness(regex, cases) > 0 ? 1 : 0))
+    + cases[1].reduce((score, _) => score + (getFitness(regex, cases) <= 0 ? 1 : 0));
 }
 
 // regex: string => number
@@ -36,7 +57,11 @@ function getComplexity(regex) {
 
 // void => string
 function getMutationType() {
-  return MUTATION_TYPES[random(0, 3)];
+  let roll = random(0, 100)
+  if (roll < 10) return MUTATION_TYPES[0];
+  if (roll < 20) return MUTATION_TYPES[1];
+  if (roll < 90) return MUTATION_TYPES[2];
+  return MUTATION_TYPES[3];
 }
 
 // seed: string => string
@@ -57,17 +82,18 @@ const cases = [
 ];
 
 let regex = '';
-for (let n = 0; n < 100; n++) {
+for (let n = 0; n < 10; n++) {
   let regex2 = regex;
   for (let n = 0; n < 10; n++) {
     regex2 = evolveRegex(regex2);
-    console.log(n, regex);
   }
   console.log('FITNESS', regex, '(' + getFitness(regex, cases) + ')', regex2, '(' + getFitness(regex2, cases) + ')')
   if (getFitness(regex2, cases) > getFitness(regex, cases)) {
     regex = regex2
   }
 }
+
+console.log(`RESULT: ${regex} (fitness=${getFitness(regex, cases)}, %correct=${getPercentCorrect(regex, cases)})`);
 
 
 
